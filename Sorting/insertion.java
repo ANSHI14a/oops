6ab50663da4047aba95f13eb166c5089392e362f
ANSHIKA SRIVASTAVA <anshikasrivastava14903@gmail.com>
2024-08-17 23:03:06 +0530

    sortsbubblesort

---
+++ Sorting/insertion.java
@@ -0,0 +1,23 @@
+package Sorting;
+
+public class insertion {
+    public static void main(String[] args) {
+        int[]arr={7,8,3,1,2};
+        //insertion sort
+        for(int i=1;i<arr.length;i++){
+            int current=arr[i];
+            int j=i-1;
+            while(j>=0 && current < arr[j]){
+                arr[j+1] =arr[j];
+                j--;
+
+
+            }
+            //placament
+            arr[j+1]=current;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
