03d2457a671fde94e799f39603176e4f0d1dd27a
ANSHIKA SRIVASTAVA <anshikasrivastava14903@gmail.com>
2024-09-10 11:50:09 +0530

    cqcollection

---
+++ Queues/cqcollection.java
@@ -0,0 +1,18 @@
+package Queues;
+import java.util.*;
+import java.util.Queue;
+public class cqcollection {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+    }
+}
